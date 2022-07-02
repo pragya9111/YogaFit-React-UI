77aab22694ec15dbf6aeeff766f1544e6a7c945b
PRAGYA BISEN <98213574+pragya9111@users.noreply.github.com>
2022-07-02 11:53:36 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,8 +1,18 @@
 import React from 'react'
+import { Routes, Route } from 'react-router-dom'
 import './App.css'
 import Nav from './Component/Navbar/Nav'
 import Home from './Component/Home/Home'
 import ExpYoga from './Component/ExpYoga/ExpYoga'
+import BookNow from './Component/BookNow/BookNow'
+import OurClasses from './Component/OurClasses/OurClasses'
+import PinkSection from './Component/PinkSection/Pink'
+import Origin from './Component/Origin/Origin'
+import Center from './Component/Center/Center'
+import Counter from './Component/Counter/Counter'
+import Blue from './Component/BlueSection/Blue'
+import Footer from './Component/Footer/Footer'
+import Contact from './Component/Contact/Contact'
 
 function App() {
   return (
@@ -10,6 +20,18 @@ function App() {
       <Nav/>
       <Home/>
       <ExpYoga/>
+      <BookNow/>
+      <OurClasses/>
+      <PinkSection/>
+      <Origin/>
+      <Center/>
+      <Counter/>
+      <Blue/>
+      <Footer/>
+      <Routes>
+      <Route path='/contact' element={<Contact/>}></Route>
+
+      </Routes>
     </div>
   )
 }
