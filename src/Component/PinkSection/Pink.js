5f0cb242d11370ca3796bb215dee7dcafe381b56
PRAGYA BISEN <98213574+pragya9111@users.noreply.github.com>
2022-07-02 17:13:04 +0530

    Update Pink.js

--- src/Component/PinkSection/Pink.js
+++ src/Component/PinkSection/Pink.js
@@ -1,16 +1,57 @@
-import React from 'react'
+import {React, useState} from 'react'
+import { useSelector } from 'react-redux'
 import './Pink.css'
 import YogaLogo from './yogalogo.png'
 
 function Pink() {
+  const [first, setfirst] = useState(1)
+  const state = useSelector(state => state.third)
+  const clickPrevious = (e) => {
+    if(first==0){
+      setfirst(2)
+    }
+    else if(first==1){
+      setfirst(0)
+    }
+    else{
+      setfirst(1)
+    }
+  }
+  const clickNext = (e) => {
+    switch (Number) {
+      case 0:
+        setfirst(1)
+        break;
+        
+      case 1:
+        setfirst(2)
+        break;
+
+      case 2:
+        setfirst(0)
+        break;
+      
+      default:
+        setfirst(0);
+        break;
+    }
+  }
+
   return (
     <div className='pinkdiv'>
-        <div className='yogalogo'>
-            <img src={YogaLogo} alt='' />
+      <div className='yogalogo'>
+        <img src={YogaLogo} alt='' />
+      </div>
+
+      <div className='para'>
+        <div className='arr1'>
+          <i class="ri-arrow-left-s-line" onClick={clickPrevious}></i>
+          <i class="ri-arrow-right-s-line" onClick={clickNext}></i>
         </div>
-        <div className='para'>
-            <p>Yoga Fit is where you can find balance, harmony and energy renewal amidst the hectic bustle of everyday pressures and deadlines. Relax & enjoy a personalized day yoga experience in our comfortable sanctuary.</p>
-        </div>
+        <p>{state[first].quote}</p>
+        <h5>{state[first].writer}</h5>
+        <h6>{state[first].profession}</h6>
+      </div>
     </div>
   )
 }
