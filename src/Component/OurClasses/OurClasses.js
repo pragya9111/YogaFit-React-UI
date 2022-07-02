18647ee69c68d97d381ada3e4924540a6e375054
PRAGYA BISEN <98213574+pragya9111@users.noreply.github.com>
2022-07-02 17:12:06 +0530

    Update OurClasses.js

--- src/Component/OurClasses/OurClasses.js
+++ src/Component/OurClasses/OurClasses.js
@@ -1,9 +1,33 @@
-import React from 'react'
+import { React, useState } from 'react'
+import { useSelector } from 'react-redux'
 import './OurClasses.css'
 
 function OurClasses() {
+  const state = useSelector(state => state.second)
+
+  const hold = state.map((elem, index) => {
+    return <div className='section' key={index} id={index}>
+      <div className='img' >
+        <img src={elem.img} alt="" />
+      </div>
+      <div className='dets'>
+        <h3>{elem.head1} </h3>
+        <p>{elem.head2} </p>
+      </div>
+    </div>
+  })
+
+
   return (
-    <div>OurClasses</div>
+    <div className='ourclasses'>
+      <div className='hd'>
+        <h1>Our Classes</h1>
+      </div>
+
+      <div className='sections'>
+        {hold}
+      </div>
+    </div>
   )
 }
 
