e47c43cc82591efbb332d2a6b13c05601895ed78
PRAGYA BISEN <98213574+pragya9111@users.noreply.github.com>
2022-07-02 18:44:05 +0530

    Delete Redux directory

--- Redux/Action/ActionType.js
+++
@@ -1,5 +0,0 @@
-export const OnPage1Load = "OnPage1Load"
-
-export const OnClassLoad = "OnClassLoad " 
- 
-export const OnPage3Load ="OnPage3Load"
--- Redux/Store.js
+++
@@ -1,8 +0,0 @@
-import {createStore, applyMiddleware} from 'redux'
-import thunk from 'redux-thunk'
-import reducers from './Reducer/CombineReducer'
-const middleware = [thunk]
-
-const store = createStore(reducers, applyMiddleware(...middleware))
-
-export default store;
