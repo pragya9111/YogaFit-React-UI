4c464e40fe5bcc0ab877fc4fc62aac32823bc048
PRAGYA BISEN <98213574+pragya9111@users.noreply.github.com>
2022-07-02 17:04:43 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -3,36 +3,30 @@ import { Routes, Route } from 'react-router-dom'
 import './App.css'
 import Nav from './Component/Navbar/Nav'
 import Home from './Component/Home/Home'
-import ExpYoga from './Component/ExpYoga/ExpYoga'
-import BookNow from './Component/BookNow/BookNow'
-import OurClasses from './Component/OurClasses/OurClasses'
-import PinkSection from './Component/PinkSection/Pink'
-import Origin from './Component/Origin/Origin'
-import Center from './Component/Center/Center'
-import Counter from './Component/Counter/Counter'
-import Blue from './Component/BlueSection/Blue'
 import Footer from './Component/Footer/Footer'
 import Contact from './Component/Contact/Contact'
+import Register from './Component/Register/Register'
 
 function App() {
   return (
     <div className='app'>
-      <Nav/>
-      <Home/>
-      <ExpYoga/>
-      <BookNow/>
-      <OurClasses/>
-      <PinkSection/>
-      <Origin/>
-      <Center/>
-      <Counter/>
-      <Blue/>
-      <Footer/>
-      <Routes>
-      <Route path='/contact' element={<Contact/>}></Route>
 
+      <Nav />
+
+      <Routes>
+        <Route path='/' element={<Home />}></Route>
+        <Route path='/contact' element={<Contact />}></Route>
+        <Route path='/register' element={<Register />}></Route>
       </Routes>
+
+      <Footer />
+
     </div>
+  )
+}
+
+
+export default App
   )
 }
 
